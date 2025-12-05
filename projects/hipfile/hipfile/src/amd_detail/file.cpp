bd66f5ff7ea8e4793bb5180b124284376f030919
Dana Robinson <43805+derobins@users.noreply.github.com>
2025-12-05 09:32:50 -0800

    Remove Buffer, File, and Stream class friends (#110)
    Instead, use the PassKey idiom to ensure that objects can only be instantiated by their containers.
    
    [ROCm/hipFile commit: 682ed73149e49bcd9ce88b9497d2087fcb15da25]

--- projects/hipfile/hipfile/src/amd_detail/file.cpp
+++ projects/hipfile/hipfile/src/amd_detail/file.cpp
@@ -6,6 +6,7 @@
 #include "context.h"
 #include "file.h"
 #include "mountinfo.h"
+#include "passkey.h"
 #include "sys.h"
 
 #include <algorithm>
@@ -68,7 +69,7 @@ IFile::getHandle() const
     return reinterpret_cast<hipFileHandle_t>(const_cast<IFile *>(this));
 }
 
-File::File(const UnregisteredFile &uf)
+File::File(const UnregisteredFile &uf, const PassKey<FileMap> &)
     : fd{uf.getFd()}, stx{uf.getStatx()}, status_flags{uf.getFlags()}, mountinfo{uf.getMountInfo()}
 {
 }
@@ -115,7 +116,7 @@ FileMap::registerFile(const UnregisteredFile &uf)
         throw FileAlreadyRegistered();
     }
 
-    auto file                  = std::shared_ptr<IFile>(new File(uf));
+    auto file                  = std::shared_ptr<IFile>(new File(uf, PassKey<FileMap>{}));
     from_fd[file->getFd()]     = file;
     from_fh[file->getHandle()] = file;
 
