b53d27de8e9102abe30dbaa021d204bb3b249430
kurtmcmillan <kurtmcmillan@users.noreply.github.com>
2025-12-10 09:40:27 -0700

    hipfile: Introduce Configuration (#118)
    ## Motivation
    
    Introduce infrastructure that will allow hipFile behavior to be
    controlled with environment variables and settings in configuration
    files.
    
    This patch series allows users to specify
    HIPFILE_ALLOW_COMPAT_MODE=false to force IO to take the AMD Infinity
    Storage (AIS) path path or, HIPFILE_FORCE_COMPAT_MODE=true to force IO
    to use the POSIX path.
    
    [ROCm/hipFile commit: 69421e13759ff1a5d32aa9e0ed3762276831222c]

---
+++ projects/hipfile/hipfile/src/amd_detail/environment.cpp
@@ -0,0 +1,36 @@
+/* Copyright (c) Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#include "environment.h"
+
+using namespace hipFile;
+using namespace std;
+
+template <>
+std::optional<bool>
+Environment::get<bool>(const char *key)
+{
+    const char *value{Context<Sys>::get()->getenv(key)};
+    if (value) {
+        if (!strcasecmp(value, "true")) {
+            return true;
+        }
+        if (!strcasecmp(value, "false")) {
+            return false;
+        }
+    }
+    return std::nullopt;
+}
+
+optional<bool>
+Environment::allow_compat_mode()
+{
+    return Environment::get<bool>(Environment::ALLOW_COMPAT_MODE);
+}
+
+optional<bool>
+Environment::force_compat_mode()
+{
+    return Environment::get<bool>(Environment::FORCE_COMPAT_MODE);
+}
