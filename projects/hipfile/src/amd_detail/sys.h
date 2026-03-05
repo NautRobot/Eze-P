9fc7ebee28500e82724140b57de822e99bc1bd89
Zach Byrne <zbyrne@amd.com>
2026-03-05 14:49:53 -0700

    Statistics reporting tool: ais-stats (#189)
    Small command line application to collect useful performance information from hipFile.
    
    Part of AIHIPFILE-99
    
    [ROCm/hipFile commit: 94659af28acf62592ea9365d1c52b7428f5f0cb6]

--- projects/hipfile/src/amd_detail/sys.h
+++ projects/hipfile/src/amd_detail/sys.h
@@ -40,9 +40,14 @@ struct Sys {
     virtual struct stat  fstat(int fd) const;
     virtual struct statx statx(int dirfd, const char *pathname, int flags, unsigned int mask) const;
 
-    virtual int fcntl(int fd, int op, uintptr_t arg) const;
+    virtual int  fcntl(int fd, int op, uintptr_t arg) const;
+    virtual void ftruncate(int fd, off_t offset) const;
 
     virtual char *getenv(const char *name) const noexcept;
+
+    virtual int memfd_create(const char *name, unsigned int flags) const;
+    virtual int eventfd(unsigned int initval, int flags) const;
+    virtual int pidfd_open(pid_t pid, unsigned int flags) const;
 };
 
 }
